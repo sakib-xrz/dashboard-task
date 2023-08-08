6b83ecc3bcaeba50a2a8033e1dc8e08063dbc1dc
sakib-xrz <sakibxrz21@gmail.com>
2023-08-08 12:19:12 +0600

    html 5 semantic tag add

--- src/lib/components/LeftSidebar/LeftSidebar.jsx
+++ src/lib/components/LeftSidebar/LeftSidebar.jsx
@@ -32,21 +32,21 @@ export default function LeftSidebar() {
             </ul>
             <div className="p-3 md:hidden">
                 <button onClick={() => setIsExpand(!expand)}
-                    class="w-full bg-transparent text-teal-600 border border-teal-600 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-between "
+                    className="w-full bg-transparent text-teal-600 border border-teal-600 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-between "
                     type="button"
                 >
                     {pathname === "/" ? "Dashboard" : pathname === "/teacher" ? "Teacher" : "Student"}
                     <ChevronDownIcon className="h-5 w-5" />
                 </button>
 
-                <div class={`z-10 ${expand ? "" : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow w-full`}>
-                    <ul class="text-sm text-gray-700">
+                <div className={`z-10 ${expand ? "" : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow w-full`}>
+                    <ul className="text-sm text-gray-700">
                         {menus.map((e, i) => (
                             <li key={i}>
                                 <Link
                                     onClick={() => setIsExpand(!expand)}
                                     href={e.path}
-                                    class={`block px-4 py-2 ${pathname === e.path
+                                    className={`block px-4 py-2 ${pathname === e.path
                                         ? " hover:bg-teal-100 bg-teal-100"
                                         : "hover:bg-gray-50"
                                         }  text-teal-700 `}
