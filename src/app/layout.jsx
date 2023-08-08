4eb99a8e1ed0a948d058874f55e1ac81a2ab820e
sakib-xrz <sakibxrz21@gmail.com>
2023-08-08 10:43:14 +0600

    html 5 semantic tag add

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,11 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={inter.className + " lg: flex"}>
-                    <LeftSidebar />
+            <body className={inter.className + " lg:flex"}>
+                <LeftSidebar />
                 <main className="lg:shrink lg:w-full p-3 lg:p-10">
-                        {children}
-                    </main>
+                    {children}
+                </main>
             </body>
         </html>
     );
