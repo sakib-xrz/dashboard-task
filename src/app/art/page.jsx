6e3790855ba3cf38fa6297004013509eb9e522ce
sakib-xrz <sakibxrz21@gmail.com>
2023-08-14 16:23:46 +0600

    Create a new component for art page table

--- src/app/art/page.jsx
+++ src/app/art/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { getArt } from "@/lib/api";
 import Modal from "@/lib/components/Modal/Modal";
+import CustomTable from "@/lib/components/Table/CustomTable";
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/solid";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
@@ -42,63 +43,11 @@ export default function Art() {
                             <AdjustmentsHorizontalIcon className="h-8 w-8" />
                         </Link>
                     </div>
-                    <div className="overflow-x-scroll lg:overflow-x-hidden">
-                        <table
-                            className={`text-sm text-gray-500 border w-full mx-auto`}
-                        >
-                            <thead className="text-teal-600 uppercase bg-teal-100">
-                                <tr>
-                                    {headers.map((header, index) => (
-                                        <th key={index} className="px-4 py-3">
-                                            {header}
-                                        </th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {tableData?.map((data, index) => (
-                                    <tr
-                                        key={index}
-                                        className="border-b text-center"
-                                    >
-                                        <td className="px-4 py-2 font-bold text-gray-800 whitespace-nowrap">
-                                            {data.id}
-                                        </td>
-                                        <td className="py-2 flex justify-center">
-                                            <Image
-                                                className="w-10 h-10 object-cover rounded-md"
-                                                src={data.image}
-                                                alt={data.title}
-                                                width={"200"}
-                                                height={"200"}
-                                            />
-                                        </td>
-                                        <td className="px-4 py-2 font-medium text-gray-800 whitespace-nowrap">
-                                            {data.title}
-                                        </td>
-                                        <td className="px-4 py-2 font-medium text-gray-800 whitespace-nowrap">
-                                            ${data.price}
-                                        </td>
-                                        <td className="px-4 py-2 font-medium text-gray-800 whitespace-nowrap">
-                                            ${data.seller_name}
-                                        </td>
-                                        <td className="px-4 py-2 font-medium text-gray-800 whitespace-nowrap">
-                                            <button
-                                                onClick={() =>
-                                                    handleViewDetailsClick(
-                                                        data.id
-                                                    )
-                                                }
-                                                className="border border-teal-500 text-teal-600 px-1 rounded-sm"
-                                            >
-                                                View Details
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
+                    <CustomTable
+                        headers={headers}
+                        tableData={tableData}
+                        handleViewDetailsClick={handleViewDetailsClick}
+                    />
                     <Modal
                         modalOpen={modalOpen}
                         setModalOpen={setModalOpen}
--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,60 +1,85 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
 
 export default function Settings() {
-  return (
-      <div>
-          <fieldset>
-              <legend className="text-lg font-semibold leading-6 text-gray-900">
-                  Select view mode
-              </legend>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                  This view will only be used for the art page.
-              </p>
-              <div className="mt-6 space-y-6">
-                  <div className="flex items-center gap-x-3">
-                      <input
-                          id="tabular"
-                          name="select-view"
-                          type="radio"
-                          className="h-4 w-4 border-gray-300 text-teal-600 focus:ring-teal-600 cursor-pointer"
-                      />
-                      <label
-                          htmlFor="tabular"
-                          className="block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
-                      >
-                          Tabular View
-                      </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                      <input
-                          id="compact"
-                          name="select-view"
-                          type="radio"
-                          className="h-4 w-4 border-gray-300 text-teal-600 focus:ring-teal-600 cursor-pointer"
-                      />
-                      <label
-                          htmlFor="compact"
-                          className="block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
-                      >
-                          Compact View
-                      </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                      <input
-                          id="card"
-                          name="select-view"
-                          type="radio"
-                          className="h-4 w-4 border-gray-300 text-teal-600 focus:ring-teal-600 cursor-pointer"
-                      />
-                      <label
-                          htmlFor="card"
-                          className="block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
-                      >
-                          Card View
-                      </label>
-                  </div>
-              </div>
-          </fieldset>
-      </div>
-  );
+    const [view, setView] = useState("");
+
+    // Load the default view mode from localStorage
+    useEffect(() => {
+        const storedView = localStorage.getItem("view");
+        if (storedView) {
+            setView(storedView);
+        } else {
+            setView("tabular");
+        }
+    }, []);
+
+    const handleViewChange = (newView) => {
+        setView(newView);
+        // Store the selected view mode in localStorage
+        localStorage.setItem("view", newView);
+    };
+
+    return (
+        <div>
+            <fieldset>
+                <legend className="text-lg font-semibold leading-6 text-gray-900">
+                    Select view mode
+                </legend>
+                <p className="mt-1 text-sm leading-6 text-gray-600">
+                    This view will only be used for the art page.
+                </p>
+                <div className="mt-6 space-y-6">
+                    <div className="flex items-center gap-x-3">
+                        <input
+                            id="tabular"
+                            name="select-view"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-teal-600 focus:ring-teal-600 outline-none cursor-pointer"
+                            checked={view === "tabular"}
+                            onChange={() => handleViewChange("tabular")}
+                        />
+                        <label
+                            htmlFor="tabular"
+                            className="block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
+                        >
+                            Tabular View
+                        </label>
+                    </div>
+                    <div className="flex items-center gap-x-3">
+                        <input
+                            id="compact"
+                            name="select-view"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-teal-600 focus:ring-teal-600 outline-none cursor-pointer"
+                            checked={view === "compact"}
+                            onChange={() => handleViewChange("compact")}
+                        />
+                        <label
+                            htmlFor="compact"
+                            className="block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
+                        >
+                            Compact View
+                        </label>
+                    </div>
+                    <div className="flex items-center gap-x-3">
+                        <input
+                            id="card"
+                            name="select-view"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-teal-600 focus:ring-teal-600 outline-none cursor-pointer"
+                            checked={view === "card"}
+                            onChange={() => handleViewChange("card")}
+                        />
+                        <label
+                            htmlFor="card"
+                            className="block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
+                        >
+                            Card View
+                        </label>
+                    </div>
+                </div>
+            </fieldset>
+        </div>
+    );
 }
